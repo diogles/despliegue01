b536856c51fadedc68c3cc2b0a3d5bd33b08f54c
diogles <jeissondpc@gmail.com>
2026-05-03 00:12:17 -0500

    Corrige servidor para Render

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
-const http=require('http')
+require("dotenv").config();
+const http = require("http");
 
-function requestController(req, res){
-    console.log('Bienvenidos al curso')
-    res.end()
-}
+const requestController = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bienvenidos al curso");
+};
 
-const server=http.createServer(requestController)
+const server = http.createServer(requestController);
 
-const PORT=process.env.PORT
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, function(){
-    console.log("Aplicacion corriendo en: " + PORT)
-})
+server.listen(PORT, () => {
+  console.log("Aplicacion corriendo en:", PORT);
+});
